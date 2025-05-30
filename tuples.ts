328c170545503e05d2544acd7bf475407fd86d66
ShaikHafiza <138603502+ShaikHafiza@users.noreply.github.com>
2025-05-30 12:48:26 +0530

    tuples

---
+++ tuples.ts
@@ -0,0 +1,31 @@
+// define our tuple
+let ourTuple: [number, boolean, string];
+
+// initialize correctly
+ourTuple = [5, false, 'Coding God was here'];
+
+console.log(ourTuple);
+
+// define our tuple
+// let ourTuple: [number, boolean, string];
+
+// // initialized incorrectly which throws an error
+// ourTuple = [false, 'Coding God was mistaken', 5];
+
+// console.log(ourTuple);
+
+
+
+// define our tuple
+let ourTuple2: [number, boolean, string];
+// initialize correctly
+ourTuple2 = [5, false, 'Coding God was here'];
+// We have no type safety in our tuple for indexes 3+
+ourTuple2.push('Something new and wrong');
+console.log(ourTuple);
+
+
+// define our readonly tuple
+const ourReadonlyTuple: readonly [number, boolean, string] = [5, true, 'The Real Coding God'];
+// throws error as it is readonly.
+// ourReadonlyTuple.push('Coding God took a day off');
